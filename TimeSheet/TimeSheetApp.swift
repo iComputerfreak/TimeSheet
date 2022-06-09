cd12066c3f0b668e4e3f1e9ecd2822a86b0b5dcc
Jonas Frey <dev@jonasfrey.de>
2022-06-09 15:39:08 +0200

    Initial Commit

---
+++ TimeSheet/TimeSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeSheetApp.swift
+//  TimeSheet
+//
+//  Created by Jonas Frey on 09.06.22.
+//
+
+import SwiftUI
+
+@main
+struct TimeSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
