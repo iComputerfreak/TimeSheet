ea80cccb79b12e0c962b497e3f5dd704b548e715
Jonas Frey <dev@jonasfrey.de>
2025-06-07 19:45:36 +0200

    Add unit test target and test plan

---
+++ TimeSheetTests/TimeSheetTests.swift
@@ -0,0 +1,10 @@
+// Copyright © 2025 Jonas Frey. All rights reserved.
+
+import Testing
+
+struct TimeSheetTests {
+    @Test func example() async throws {
+        // TODO: Implement tests
+        #expect(true)
+    }
+}
