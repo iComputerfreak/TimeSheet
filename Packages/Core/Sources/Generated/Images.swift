a9dc68ea5182b2251b8a88ea59a0393efd434205
Jonas Frey <dev@jonasfrey.de>
2025-06-08 18:49:55 +0200

    Remove test image

--- Packages/Core/Sources/Generated/Images.swift
+++ Packages/Core/Sources/Generated/Images.swift
@@ -8,22 +8,10 @@ private let bundle = Bundle(for: BundleToken.self)
 // MARK: - Asset Catalogs
 
 public enum Images {
-    public static let testImage = ImageAsset(name: "testImage")
 }
 
 // MARK: - Implementation Details
 
-public struct ImageAsset: Sendable {
-    fileprivate let name: String
-
-    public var image: Image {
-        return Image(name, bundle: bundle)
-    }
-
-    public var uiImage: UIImage {
-        return UIImage(named: name, in: bundle, with: nil)!
-    }
-}
 
 private final class BundleToken {}
 
